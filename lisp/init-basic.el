6ecb52ac6a7f9019456bddc38d07dcd9316007ea
P0ng64 <56666973+P0ng64@users.noreply.github.com>
2021-12-29 14:39:59 +0800

    Set mac command key as Meta

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -47,10 +47,14 @@
     (setq w32-lwindow-modifier 'super     ; Left Windows key
           w32-apps-modifier 'hyper)       ; Menu/App key
     (w32-register-hot-key [s-t]))
+   (sys/mac-ns-p
+    (setq ns-command-modifier 'meta
+          ns-option-modifier 'super))
    (sys/mac-port-p
     ;; Compatible with Emacs Mac port
-    (setq mac-option-modifier 'meta
-          mac-command-modifier 'super)
+
+    (setq mac-option-modifier 'super
+          mac-command-modifier 'meta)
     (bind-keys ([(super a)] . mark-whole-buffer)
                ([(super c)] . kill-ring-save)
                ([(super l)] . goto-line)
