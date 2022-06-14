5e78538b0752c992c98b965d21f54d3de99467d6
P0ng64 <56666973+P0ng64@users.noreply.github.com>
2022-06-14 14:34:59 +0800

    Merge upstream.

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -290,7 +290,6 @@ Also, delete any process that is exited or signaled."
 (when (display-graphic-p)
   (add-hook 'window-setup-hook #'fix-fullscreen-cocoa)
   (bind-key "S-s-<return>" #'toggle-frame-fullscreen)
->>>>>>> seagle0128-master
   (and sys/mac-x-p (bind-key "C-s-f" #'toggle-frame-fullscreen))
 
   ;; Resize and re-position frames conveniently
