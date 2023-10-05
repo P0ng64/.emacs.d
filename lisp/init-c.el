3dfc0a88fbd1536c753f101052d88c7f7cadd7a3
Zhipeng Bao <p0ng.64@yahoo.com>
2023-10-05 15:03:35 +0800

    Custom C code style

--- lisp/init-c.el
+++ lisp/init-c.el
@@ -38,11 +38,10 @@
   :ensure nil
   :bind (:map c-mode-base-map
          ("<f12>" . compile))
-  :init (setq-default c-basic-offset 4))
+  :init (setq c-default-style "gnu"))
 
 (when (and centaur-tree-sitter (centaur-treesit-available-p))
-  (use-package c-ts-mode
-    :init (setq c-ts-mode-indent-offset 4)))
+  (use-package c-ts-mode)
 
 (provide 'init-c)
 
